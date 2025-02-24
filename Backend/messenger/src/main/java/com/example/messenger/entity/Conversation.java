87dfae3aed1e5340e19ec8ace9e51cfb59c0c89a
c0de-saurabh <saurabhkumarw.990sk@gmail.com>
2025-02-24 17:14:55 +0530

    Added Conversation Schema

--- Backend/messenger/src/main/java/com/example/messenger/entity/Conversation.java
+++ Backend/messenger/src/main/java/com/example/messenger/entity/Conversation.java
@@ -1,4 +1,28 @@
 package com.example.messenger.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "conversations")
+@Getter @Setter
 public class Conversation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    boolean isGroupChat;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+
 }
--- Backend/messenger/src/main/java/com/example/messenger/entity/Participant.java
+++ Backend/messenger/src/main/java/com/example/messenger/entity/Participant.java
@@ -1,4 +1,6 @@
 package com.example.messenger.entity;
 
 public class Participant {
+
+
 }

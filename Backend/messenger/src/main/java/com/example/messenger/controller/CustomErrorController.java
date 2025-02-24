982cfbf5e06bb248b211a9d1feb812dd97be7add
c0de-saurabh <saurabhkumarw.990sk@gmail.com>
2025-02-24 14:01:22 +0530

    Fixed Redirecting - Security Config and CSRF Protection added,SecurityContextHolder used for clearing session from app only

---
+++ Backend/messenger/src/main/java/com/example/messenger/controller/CustomErrorController.java
@@ -0,0 +1,19 @@
+package com.example.messenger.controller;
+
+import org.springframework.beans.factory.support.ManagedProperties;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public ResponseEntity<Map<String, String>> handleError() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Unauthorized or Session Expired"));
+    }
+}
+
